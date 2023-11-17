44915c99a4fe124828a2728d07b4e2c756b175b2
DynoXD <122469427+Dyno-XD@users.noreply.github.com>
2023-11-17 22:14:11 +0300

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,9 @@
-import os, sys
-print(" Update Chaking...")
+import os, sys, time
+G = "\u001b[32;1m"
+WH = "\u001b[0m"
+Y = "\u001b[33;1m"
+print("{Y} Update Chaking...")
+time.sleep(1.5)
+print("{G} Update Success ✓ {WH} ")
 os.system(" git pull")
 import SUPER
